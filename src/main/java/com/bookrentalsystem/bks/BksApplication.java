4fe43c22421364e1e20c5a52ea2b1c91b7f01e37
manishpudasaini <eziomanish1@gmail.com>
2023-06-14 10:27:23 +0545

    Book rental System - Project setup added all the dependency

---
+++ src/main/java/com/bookrentalsystem/bks/BksApplication.java
@@ -0,0 +1,13 @@
+package com.bookrentalsystem.bks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookrentalsystem/bks/BksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookrentalsystem.bks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
