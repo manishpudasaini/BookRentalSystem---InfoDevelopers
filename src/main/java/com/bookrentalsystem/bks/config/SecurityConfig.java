9dd3a2b418f278eec07b7490f67c6dae684684e0
manishpudasaini <eziomanish1@gmail.com>
2023-07-02 11:25:10 +0545

    jwt configuration : Book Rental System - restApiJwt branch

--- src/main/java/com/bookrentalsystem/bks/config/SecurityConfig.java
+++ src/main/java/com/bookrentalsystem/bks/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.bookrentalsystem.bks.config;
 
-import com.bookrentalsystem.bks.config.jwtFilter.JwtFilter;
 import com.bookrentalsystem.bks.service.UserDetailService.MyUserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -13,12 +12,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -26,7 +23,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final JwtFilter jwtFilter;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
